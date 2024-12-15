7c0770cad5a97245d486fc52792fa10996c0e411
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-12-15 16:36:48 +0530

    finds the csv encoding automatically now. added a readme to document the issue and how to fix it.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+the last two / three lines needs to be delted from the hwinfo64
+csv file as they contain information about the hardware, which will
+cause problem with the program.
--- visualizeHWInfo.py
+++ visualizeHWInfo.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import chardet
 
 # get current directory location
 current_dir = os.getcwd()
 
 # get list of files in current directory
-current_dir_files = [item for item in os.listdir(current_dir) if os.path.isfile(os.path.join(current_dir, item)) and item.endswith(".csv")]
+current_dir_files = [item for item in os.listdir(current_dir) if os.path.isfile(os.path.join(current_dir, item)) and (item.endswith(".csv") or item.endswith(".CSV"))]
 
 # display the list to the user
+if len(current_dir_files) == 0:
+    print("No csv files found!!!. Exiting...")
+    os._exit()
 for (idx, file) in enumerate(current_dir_files):
     print(f"{idx}) {file} \n")
 
@@ -17,13 +21,19 @@ print("Please enter 1 option: \n")
 selected_file_option = None
 try:
     selected_file_option = int(input())
-except e:
-    print(e)
+except ValueError:
+    print("Value Error")
     os._exit()
 print(f"The selected option is: {selected_file_option}")
+selected_file_path = os.path.join(current_dir, current_dir_files[selected_file_option])
+
+# get the csv file encoding
+with open(selected_file_path, 'rb') as file:
+    file_encodig = chardet.detect(file.read())
+print(file_encodig)
 
 # load the file using pandas
-selected_csv_file = pd.read_csv(os.path.join(current_dir, current_dir_files[selected_file_option]))
+selected_csv_file = pd.read_csv(selected_file_path, encoding=file_encodig["encoding"])
 
 # get all the columns and display it to the user
 # ask the user to select 1 or more columns from the list
@@ -32,8 +42,8 @@ for (idx, col_name) in enumerate(selected_csv_file.columns):
     print(f"{idx} {col_name}")
 try:
     selected_columns = list(map(int, input().split()))
-except e:
-    print(e)
+except ValueError:
+    print("Value Error")
     os._exit()
 print(f"The selected columns are: {selected_columns}")
 
